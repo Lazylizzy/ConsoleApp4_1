4cbdd73f649f6ed3303919f43b608c0d6e57892d
Elizaveta <Elizaveta@DESKTOP-T76K9LI>
2021-10-18 21:52:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N больше 0: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int S = 0;
+            for (int i = 1; i < N+1; i++)
+            {
+                S = S + (2 * i - 1);
+                Console.WriteLine("{0} ", S);               
+            
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
